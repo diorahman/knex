9b3cf33e543126f43fad36f083b799c2c53d46f7
Mohammad Anwari <mdamt@hooq.tv>
2016-11-26 18:41:31 +0800

    Set connection to connectionSettings

--- src/dialects/postgres/index.js
+++ src/dialects/postgres/index.js
@@ -106,7 +106,7 @@ assign(Client_PG.prototype, {
         connection = new client.driver.Client(index > 0 ? client.readReplicaConnectionSettings1 : client.readReplicaConnectionSettings);
       }
       else {
-        connection = client.connectionSettings;
+        connection = new client.driver.Client(client.connectionSettings);
       }
       connection.connect(function(err, connection) {
         if (err) {
